cd1c2ede0cc8087aa92284a46c7b4f75e4d5c1d7
BTheofil <btheofil7@gmail.com>
2026-09-12 20:14:44 +0200

    add log to firebase server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "hu.tb"
-version = "1.3.0"
+version = "1.3.1"
 
 kotlin {
     jvmToolchain(libs.versions.java.get().toInt())
--- server/src/main/kotlin/hu/tb/meet/notification/FirebasePushSender.kt
+++ server/src/main/kotlin/hu/tb/meet/notification/FirebasePushSender.kt
@@ -4,11 +4,26 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.MessagingErrorCode
 import com.google.firebase.messaging.MulticastMessage
+import org.slf4j.LoggerFactory
 
 class FirebasePushSender(private val app: FirebaseApp) : PushSender {
 
+    private val logger = LoggerFactory.getLogger(FirebasePushSender::class.java)
+
     override fun send(fids: List<String>, message: MulticastMessage): List<String> {
         val response = FirebaseMessaging.getInstance(app).sendEachForMulticast(message)
+
+        response.responses.forEachIndexed { index, result ->
+            val exception = result.exception ?: return@forEachIndexed
+
+            logger.warn(
+                "push to fid {} rejected: {} - {}",
+                fids[index],
+                exception.messagingErrorCode,
+                exception.message
+            )
+        }
+
         return response.responses.withIndex()
             .filter { (_, it) -> it.exception?.messagingErrorCode == MessagingErrorCode.UNREGISTERED }
             .map { (index, _) -> fids[index] }
